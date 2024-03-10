1716f27f7e7b695f080eacdd85eb356e83449a9e
Mojtaba Michael <mojimich2015@gmail.com>
2024-03-10 10:11:12 +0300

    Made host and port dynamic using template injection

--- chat/main.go
+++ chat/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"html/template"
 	"log"
 	"net/http"
@@ -21,16 +22,19 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 				filepath.Join("templates", t.filename),
 			),
 		)
-		t.templ.Execute(w, nil)
+		t.templ.Execute(w, r)
 	})
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "The addr of the application.")
+	flag.Parse()
 	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 	go r.run()
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Println("Starting web server on", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
